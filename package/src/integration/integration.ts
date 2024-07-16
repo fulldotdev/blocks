915d52e06df970b7704921d99e72c3245d460c3c
Sil <silveltman@gmail.com>
2024-07-16 23:59:47 +0200

    cloudcannon config, bookshop and previews integration done

--- package/src/integration/integration.ts
+++ package/src/integration/integration.ts
@@ -1,18 +1,19 @@
 import type { AstroIntegration } from 'astro'
 import merge from 'deepmerge'
 // @ts-ignore
+import yaml from 'js-yaml'
+// @ts-ignore
 import fs from 'fs'
+// @ts-ignore
+import path from 'path'
+// @ts-ignore
+import { fileURLToPath } from 'url'
 
 export default function fulldevBlocksIntegration(): AstroIntegration {
   return {
     name: '/integration',
     hooks: {
-      'astro:config:setup': async ({
-        // config: astroConfig,
-        updateConfig,
-        injectScript,
-        injectRoute,
-      }) => {
+      'astro:config:setup': async ({ injectRoute }) => {
         const pages = import.meta.glob('/src/pages/**/*.astro')
         !pages['/src/pages/404.astro'] &&
           injectRoute({
@@ -83,8 +84,115 @@ export default function fulldevBlocksIntegration(): AstroIntegration {
 
         fs.writeFileSync(
           './.cloudcannon/cloudcannon.config.json',
-          JSON.stringify(merged)
+          JSON.stringify(merged, null, 2)
         )
+      },
+      'astro:build:done': async () => {
+        // ----------------------
+        // Process cloudcannon config file
+        // ----------------------
+        const cloudcannonLib = import.meta.glob('../cloudcannon.config.yml', {
+          as: 'raw',
+          eager: true,
+        })['../cloudcannon.config.yml']
+        const cloudcannonUser = import.meta.glob('/cloudcannon.config.yml', {
+          as: 'raw',
+          eager: true,
+        })['/cloudcannon.config.yml']
+
+        const merged = merge(
+          cloudcannonLib ? yaml.load(cloudcannonLib) : {},
+          cloudcannonUser ? yaml.load(cloudcannonUser) : {}
+        )
+
+        const mergedYaml = yaml.dump(merged)
+        fs.writeFileSync('./.cloudcannon/cloudcannon.config.yml', mergedYaml)
+
+        // ----------------------
+        // Process bookshop files
+        // ----------------------
+        const libBookshops = import.meta.glob(
+          '../components/blocks/**/*.bookshop.yml',
+          {
+            as: 'raw',
+            eager: true,
+          }
+        )
+        const userBookshops = import.meta.glob(
+          '/src/components/**/*.bookshop.yml',
+          {
+            as: 'raw',
+            eager: true,
+          }
+        )
+
+        Object.entries(libBookshops).map(async ([key, value]) => {
+          const modifiedKey = key.replace(
+            '../components/blocks/',
+            '/src/components/'
+          )
+          const filename = modifiedKey.replace('/src/components/', '')
+
+          if (userBookshops[modifiedKey]) {
+            const libData = yaml.load(value)
+            const userData = yaml.load(userBookshops[modifiedKey])
+            const merged = { ...libData, ...userData }
+            const mergedYaml = yaml.dump(merged)
+            fs.writeFileSync(
+              `./.cloudcannon/components/${filename}`,
+              mergedYaml
+            )
+          } else {
+            fs.writeFileSync(`./.cloudcannon/components/${filename}`, value)
+          }
+        })
+
+        Object.entries(userBookshops).map(async ([key, value]) => {
+          const modifiedKey = key.replace(
+            '/src/components/',
+            '../components/blocks/'
+          )
+          const filename = modifiedKey.replace('../components/blocks/', '')
+
+          if (!userBookshops[modifiedKey]) {
+            fs.writeFileSync(`./.cloudcannon/components/${filename}`, value)
+          }
+        })
+
+        // ----------------------
+        // Process bookshop previews
+        // ----------------------
+
+        const __filename = fileURLToPath(import.meta.url)
+        const __dirname = path.dirname(__filename)
+        const getAbsolutePath = (relativePath: any) =>
+          path.resolve(__dirname, relativePath)
+
+        const libPreviews = import.meta.glob(
+          '../components/blocks/**/*.preview.png',
+          {
+            eager: true,
+          }
+        )
+
+        const userPreviews = import.meta.glob(
+          '/src/components/**/*.preview.png',
+          {
+            eager: true,
+          }
+        )
+
+        Object.entries(libPreviews).forEach(([key, value]) => {
+          const filename = key.replace('../components/blocks/', '')
+          const absolutePath = getAbsolutePath(key)
+          fs.copyFileSync(absolutePath, `./.cloudcannon/components/${filename}`)
+        })
+
+        Object.entries(userPreviews).forEach(([key, value]) => {
+          const filename = key.replace('/src/components/', '')
+          const absolutePath = getAbsolutePath(key)
+          fs.copyFileSync(absolutePath, `./.cloudcannon/components/${filename}`)
+        })
       },
     },
   }
